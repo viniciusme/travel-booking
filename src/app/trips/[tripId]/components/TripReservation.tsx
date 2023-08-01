92a8fb8f07fdcf2629a33ef71b30c6bbd9e52176
Vini Mendes <vinimendesdev@gmail.com>
2023-07-31 21:47:07 -0300

    style: add border  to trip reservation component

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -11,33 +11,33 @@ interface TripReservationProps {
 
 const TripReservation = ({ trip }: TripReservationProps) => {
   return (
-    <div>
-      <div className='flex flex-col px-5'>
-        <div className='flex gap-4'>
-          <DatePicker
-            placeholderText='Data Início'
-            onChange={() => {}}
-            className='w-full'
-          />
-          <DatePicker
-            placeholderText='Data Fim'
-            onChange={() => {}}
-            className='w-full'
-          />
-        </div>
-
-        <Input
-          type='number'
-          placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
-          className='mt-4'
+    <div className='flex flex-col px-5'>
+      <div className='flex gap-4'>
+        <DatePicker
+          placeholderText='Data Início'
+          onChange={() => {}}
+          className='w-full'
         />
+        <DatePicker
+          placeholderText='Data Fim'
+          onChange={() => {}}
+          className='w-full'
+        />
+      </div>
 
-        <div className='flex justify-between mt-3'>
-          <p className='font-medium text-sm text-primaryDarker'>Total: </p>
-          <p className='font-medium text-sm text-primaryDarker'>R$2500</p>
-        </div>
+      <Input
+        type='number'
+        placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
+        className='mt-4'
+      />
 
-        <Button className='mt-4'>Reservar Agora</Button>
+      <div className='flex justify-between mt-3'>
+        <p className='font-medium text-sm text-primaryDarker'>Total: </p>
+        <p className='font-medium text-sm text-primaryDarker'>R$2500</p>
+      </div>
+
+      <div className='pb-10 border-b border-grayLighter w-full'>
+        <Button className='mt-4 w-full'>Reservar Agora</Button>
       </div>
     </div>
   );
